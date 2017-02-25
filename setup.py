62260d0037b53e674632d56558488610a6de780e
Jks Liu <chinatianma@gmail.com>
2017-02-25 13:15:25 +0800

    v1.0.1 add document for production release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(MODULE_PATH, 'README.txt')) as f:
 
 setup(
     name='rer',
-    version='0.0.1',
+    version='1.0.1',
     description='Regular expression in regular expression',
     long_description=LONG_DESCRIPTION,
 
@@ -30,7 +30,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
